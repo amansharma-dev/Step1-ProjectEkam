b9f38cd2d23b8d8703b59d2e685b4e2cf07750e7
amansharma-dev <amansharma.11@hotmail.com>
2020-03-30 17:51:21 +0530

    commit

--- app/src/main/java/com/example/step1_projectekam/MainActivity.java
+++ app/src/main/java/com/example/step1_projectekam/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.step1_projectekam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+    private RequestQueue queue;
+    public static final  String url = "https://api.chucknorris.io/jokes/random";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text);
+
+        queue = MySingleton.getInstance(this.getApplicationContext()).
+                getRequestQueue();
+
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Log.d("Response", "onResponse: "+response.getString("value"));
+                            textView.setText(response.getString("value"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(jsonObjectRequest);
     }
+
+
 }
