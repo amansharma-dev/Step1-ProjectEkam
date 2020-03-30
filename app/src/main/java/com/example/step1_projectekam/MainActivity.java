69f248048b2f1b4e90a98c8008c625c20666c91c
amansharma-dev <amansharma.11@hotmail.com>
2020-03-30 23:46:25 +0530

    commit

--- app/src/main/java/com/example/step1_projectekam/MainActivity.java
+++ app/src/main/java/com/example/step1_projectekam/MainActivity.java
@@ -1,47 +1,57 @@
 package com.example.step1_projectekam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
+
 import com.android.volley.toolbox.JsonObjectRequest;
 
-import org.json.JSONArray;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
 
     private TextView textView;
-    private RequestQueue queue;
+    private CardView cardView;
+    private Button updateButton;
+
+
     public static final  String url = "https://api.chucknorris.io/jokes/random";
+    private RequestQueue queue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.text);
+        textView = findViewById(R.id.funnyJokes_textView);
+        cardView = findViewById(R.id.card_cardView);
 
-        queue = MySingleton.getInstance(this.getApplicationContext()).
-                getRequestQueue();
+        queue = MySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
 
-        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
                 url,
                 null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            Log.d("Response", "onResponse: "+response.getString("value"));
+                            //textView.setText(response.getString("value"));
+                            Log.d("CNAPI", "onResponse: "+response.getString("value"));
                             textView.setText(response.getString("value"));
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -53,9 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
         queue.add(jsonObjectRequest);
     }
-
 
 }
